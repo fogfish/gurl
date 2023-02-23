89f4523a8dcf26fdb66b64e2cff4daa2c23bffd2
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2023-02-24 00:18:48 +0200

    helper executor for http.Arrow

---
+++ http/once.go
@@ -0,0 +1,93 @@
+//
+// Copyright (C) 2019 - 2023 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/gurl
+//
+
+package http
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"reflect"
+	"runtime"
+	"time"
+
+	"github.com/fogfish/gurl/v2"
+)
+
+type Status struct {
+	ID       string `json:"id"`
+	Status   string `json:"status"`
+	Duration string `json:"duration"`
+	Reason   string `json:"reason,omitempty"`
+	Payload  string `json:"payload"`
+}
+
+// Evaluates sequence of tests, returns status object for each
+func Once(tests ...func() Arrow) []Status {
+	stack := New(Memento())
+	status := make([]Status, len(tests))
+
+	for i, test := range tests {
+		arr := test()
+		ctx := stack.WithContext(context.Background())
+
+		t := time.Now()
+		err := ctx.IO(arr)
+		status[i] = newStatus(ctx, arrowName(test), time.Since(t), err)
+	}
+
+	return status
+}
+
+func WriteOnce(w io.Writer, tests ...func() Arrow) error {
+	seq := Once(tests...)
+
+	if bytes, err := json.MarshalIndent(seq, "", "  "); err == nil {
+		if _, err := w.Write(bytes); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if _, err := w.Write([]byte{'[', ']'}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func newStatus(ctx *Context, id string, dur time.Duration, err error) Status {
+	switch v := (err).(type) {
+	case nil:
+		return Status{
+			ID:       id,
+			Status:   "success",
+			Duration: dur.String(),
+			Payload:  string(ctx.Payload),
+		}
+	case *gurl.NoMatch:
+		return Status{
+			ID:       id,
+			Status:   "failure",
+			Duration: dur.String(),
+			Reason:   v.Diff,
+			Payload:  string(ctx.Payload),
+		}
+	default:
+		return Status{
+			ID:       id,
+			Status:   "failure",
+			Duration: dur.String(),
+			Reason:   err.Error(),
+			Payload:  string(ctx.Payload),
+		}
+	}
+}
+
+func arrowName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
