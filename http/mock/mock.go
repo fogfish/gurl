89b3b8beb038347c5e4839db782c548debe7f5b6
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2024-04-20 23:57:35 +0300

    update license

--- http/mock/mock.go
+++ http/mock/mock.go
@@ -1,3 +1,11 @@
+//
+// Copyright (C) 2019 - 2024 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/gurl
+//
+
 package mock
 
 import (
