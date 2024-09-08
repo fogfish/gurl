32bb9b5f08f036c2c99540596c9528ff4bce9996
fogfish <dmkolesnikov@gmail.com>
2024-09-08 20:51:39 +0300

    enable CI/CD for Golang mono repo (#65)

---
+++ x/xhtml/version.go
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2019 - 2024 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/gurl
+//
+
+package xhtml
+
+const Version = "x/xhtml/v0.0.2"
